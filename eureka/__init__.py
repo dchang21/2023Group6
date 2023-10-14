2e86ede05ebfba1d5eb663104d83b1b189649b13
drkwitht <derektan3510@gmail.com>
2023-10-14 09:20:18 -0700

    Reformatted and commented init file.

--- eureka/__init__.py
+++ eureka/__init__.py
@@ -1,14 +1,21 @@
 """
-VIA https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/
-$ flask --app eureka run
-http://127.0.0.1:5000/hello
-"/hello" comes from our route. @app.route('/hello')
+    __init__.py
+    Entry point for the EurekaEats application. See comment after imports for running steps.
 """
+
 import os
 from flask import Flask
 
+# Running Steps:
+# VIA https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/
+# $ flask --app eureka run
+# http://127.0.0.1:5000/hello
+# "/hello" comes from our route. @app.route('/hello')
+
 def create_app(test_config=None):# can change nape of "app"
     app = Flask(__name__, instance_relative_config=True)
+    
+    # NOTE: The quickstart tutorial implies that this config code to link the database is more for SQLite3 rather than MongoDB. We must replace this with PyMongo.
     app.config.from_mapping(
         SECRET_KEY='dev', #Development key only.
         DATABASE=os.path.join(app.instance_path, 'eurekaeats.mongodb')
@@ -24,8 +31,8 @@ def create_app(test_config=None):# can change nape of "app"
     except OSError:
         pass
 
-    @app.route('/hello') 
-    def hello():
+    @app.route('/home') 
+    def home():
         return 'Eureka!'
     
     return app
