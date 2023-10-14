2b87a5dd10cd6fa0120c7149f95b91a273d93e6a
drkwitht <derektan3510@gmail.com>
2023-10-14 09:21:02 -0700

    Added config file for application.

---
+++ eureka/config.py
@@ -0,0 +1,13 @@
+"""
+    config.py
+    Stores settings for the EurekaEats Flask app.
+"""
+
+# DEBUG flag: do not set to True on deployment.
+DEBUG = True
+
+# TESTING flag: should be enabled for testing.
+# TESTING = False
+
+# SESSION_COOKIE_HTTPONLY flag: optional, but can secure site cookies from malicious JavaScript.
+# SESSION_COOKIE_HTTPONLY = True 
