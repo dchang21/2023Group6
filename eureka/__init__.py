f33a632e9c2e00b2ce53dec888f5b35b0acf4aac
davidtabor0 <dtabor@calstatela.edu>
2023-10-18 18:42:21 -0700

    "users" may be stored into the DB.

--- eureka/__init__.py
+++ eureka/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):# can change nape of "app"
     app = Flask(__name__, instance_relative_config=True)
     client = MongoClient('localhost', 27017)
     user_db = client.flask_db
-    todosTEST = user_db.todosTEST
+    userCOLL = user_db.userCOLL #User Collection
     # NOTE: The quickstart tutorial implies that this config code to link the database is more for SQLite3 rather than MongoDB. We must replace this with PyMongo.
     app.config.from_mapping(
         SECRET_KEY='dev', #Development key only.
@@ -31,11 +31,11 @@ def create_app(test_config=None):# can change nape of "app"
     @app.route('/index', methods=('GET', 'POST')) 
     def index():
         if request.method=='POST':
-            username = request.form['username']
-            newuser = request.form['newuser']
-            todosTEST.insert_one({'username': username, 'newuser': newuser})
-            return redirect(url_for('index'))
+            username = request.form['username']  #Sets variable to whatever -
+            newuser = request.form['newuser']    #  -is returned from the "Value" field in radio buttons.
+            userCOLL.insert_one({'username': username, 'newuser': newuser}) # Inserts values into Collection object.
+            return redirect(url_for('index')) #Refreshes the page
         
-        all_todos = todosTEST.find()
-        return render_template('index.html', test_message="Hello World!", todosTEST=all_todos)
+        myuser = userCOLL.find_one() #Grabs first item from collection.
+        return render_template('index.html', test_message="Hello World!", test_user=myuser)
     return app
