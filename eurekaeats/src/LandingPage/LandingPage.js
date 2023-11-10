a3de78630e8584604a543f50f5d8f3d3e3075e7c
drkwitht <derektan3510@gmail.com>
2023-11-10 00:08:20 -0800

    Fixed import error.

--- eurekaeats/src/LandingPage/LandingPage.js
+++ eurekaeats/src/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Outlet from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import './LandingPage.css';
 import logo from '../assets/EurekaEatsWText.png';
 import search from '../assets/searchIcon.png';
