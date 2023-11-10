fbe922db950a56b38d26d4adea614e8e5600191e
drkwitht <derektan3510@gmail.com>
2023-11-09 21:59:47 -0800

    Added tentative React routing JSX.

--- eurekaeats/src/App.jsx
+++ eurekaeats/src/App.jsx
@@ -1,18 +1,22 @@
 
 import React from 'react';
-import Navigator from '../src/Navigator/Navigator';
+// import Navigator from '../src/Navigator/Navigator';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
+import LogIn from './LogIn/LogIn';
+// import SignIn from './SignIn/SignIn';
 
+// TODO: complete SignIn page component functionality... -DrkWithT
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <LandingPage />
-    </div>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/login" element={<LogIn/>}></Route>
+        {/* <Route path="/signin" element={<SignIn/>}></Route> */}
+      </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
