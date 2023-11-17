a24ad79a91e4389ab847f18250083707b94a1a36
drkwitht <derektan3510@gmail.com>
2023-11-16 20:09:00 -0800

    Removed old template handler.

--- eureka/__init__.py
+++ eureka/__init__.py
@@ -16,14 +16,14 @@ def create_app(test_config=None):# can change nape of "app"
     app = Flask(__name__, instance_relative_config=True)
     
     # 1b. Configure application.
-    client = MongoClient('localhost', 27017)
-    user_db = client.flask_db
-    userCOLL = user_db.userCOLL #User Collection
+    # client = MongoClient('localhost', 27017)
+    # user_db = client.flask_db
+    # userCOLL = user_db.userCOLL #User Collection
     # NOTE: The quickstart tutorial implies that this config code to link the database is more for SQLite3 rather than MongoDB. We must replace this with PyMongo.
-    app.config.from_mapping(
-        SECRET_KEY='dev', #Development key only.
-        DATABASE=os.path.join(app.instance_path, 'eurekaeats.mongodb')
-        )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev', #Development key only.
+    #     DATABASE=os.path.join(app.instance_path, 'eurekaeats.mongodb')
+    #     )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -39,14 +39,4 @@ def create_app(test_config=None):# can change nape of "app"
     app.register_blueprint(restaurant_api_router)
     app.register_blueprint(user_api_router)
 
-    @app.route('/index', methods=('GET', 'POST')) 
-    def index():
-        if request.method=='POST':
-            username = request.form['username']  #Sets variable to whatever -
-            newuser = request.form['newuser']    #  -is returned from the "Value" field in radio buttons.
-            userCOLL.insert_one({'username': username, 'newuser': newuser}) # Inserts values into Collection object.
-            return redirect(url_for('index')) #Refreshes the page
-        
-        myuser = userCOLL.find_one() #Grabs first item from collection.
-        return render_template('index.html', test_message="Hello World!", test_user=myuser)
     return app
