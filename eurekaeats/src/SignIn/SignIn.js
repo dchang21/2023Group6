47a4b8e27b0c0705943def8bdb492ae219d4095d
drkwitht <derektan3510@gmail.com>
2023-11-18 10:09:59 -0800

    Auto reformatted SignIn.

--- eurekaeats/src/SignIn/SignIn.js
+++ eurekaeats/src/SignIn/SignIn.js
@@ -1,36 +1,34 @@
 import React, { useState } from 'react';
-import {Navigate, Outlet} from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import '../index.css';
 import './SignIn.css';
-import logo from '../assets/EurekaEatsWText.png';
 import logo1 from '../assets/EurekaEatsLogo.png';
-
 
 import useJToken from '../utils/useJToken'; // For ESLint of LogIn doc comment.
 
 /**
- * @description Stateful page component for login. Checks the user session token with a to-backend call before further routing.
- * @param {useJToken} param0 
+ * @description Stateful page component for initial sign-in. Checks the user session token with a to-backend call before further routing.
+ * @param {{usedJTokenHook: useJToken}} param0 
  */
-function SignIn({useJTokenHook}) {
+function SignIn({ usedJTokenHook }) {
   /* State of login component: stores user inputs. */
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [password, setPassword] = useState();
-  const {setToken, token} = useJTokenHook();
+  const { setToken, token } = usedJTokenHook();
 
   if (token !== 'guest') {
-    return <Navigate to="/"/>;
+    return <Navigate to="/" />;
   }
 
   /**
    * @description Helper function for making an AJAX call to the app's API in order to process the login.
-   * @param {{username: string, email: string, firstName: string, lastName: string, password: string}} credentials
-   * @returns {{payload: number, data: {token: string}} | null}
+   * @param {{firstName: string, lastName: string, username: string, email: string, firstName: string, lastName: string, password: string}} credentials
+   * @returns {{payload: number, data: {token: string} | null} | null}
    */
   const doSignInCall = async (credentials) => {
     return fetch('http://127.0.0.1:5000/api/users/action', {
@@ -39,24 +37,29 @@ function SignIn({useJTokenHook}) {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({action: 36, args: credentials})
+      body: JSON.stringify({ action: 34, args: credentials })
     })
-    .then((response) => {
-      return response.json();
-    }).catch((error) => {
-      console.error(`eurekaeats [API Error]: ${error}`);
-      return null;
-    });
+      .then((response) => {
+        return response.json();
+      }).catch((error) => {
+        console.error(`eurekaeats [API Error]: ${error}`);
+        return null;
+      });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    // All fields are required.
+    if (!username || !email || !firstName || !lastName || !password) {
+      return false;
+    }
+
     const responseData = await doSignInCall({
-      username,
-      email,
       firstName,
       lastName,
+      username,
+      email,
       password
     });
 
@@ -76,49 +79,47 @@ function SignIn({useJTokenHook}) {
       setToken(null);
       console.log(`[eurekaeats]: set guest token from ${JSON.stringify(responseData)}`);
     }
+
+    return true;
   };
 
   return (
     <>
       <div>
-      <header>
-      
-      </header>
-      <main className="main-content-login">
-      <h2>Sign Up for EurekaEats</h2>
+        <main className="main-content-login">
+          <h2>Sign Up for EurekaEats</h2>
           <Link to="/">
-          <img src={logo1} alt="logo" className = "logo"/>
+            <img src={logo1} alt="logo" className="logo" />
           </Link>
           <Link to="/login">
-          <p>Already have an account? Log in here!</p>
+            <p>Already have an account? Log in here!</p>
           </Link>
           <form onSubmit={handleSubmit}>
-          <div className="form-field-bar">
-              <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}/>
+            <div className="form-field-bar">
+              <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
             </div>
             <div className="form-field-bar">
-            <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)}/>
+              <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
             </div>
             <div className="form-field-bar">
-              <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)}/>
+              <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div className="form-field-bar">
               <input type="text" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="form-field-bar">
-              <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+              <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
             </div>
             <input type="submit" className="form-login-button" />
           </form>
-      <div className="go-back-text">
-        <Link to="/">
-      <p>Made a wrong turn? Head back to the home page</p>
+          <div className="go-back-text">
+            <Link to="/">
+              <p>Made a wrong turn? Head back to the home page</p>
             </Link>
-         
           </div>
         </main>
       </div>
-      <Outlet/>
+      <Outlet />
     </>
   );
 }
