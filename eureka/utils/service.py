2f4635fef14191040c54e666addfb3abbe12ad7c
drkwitht <derektan3510@gmail.com>
2023-11-17 21:35:13 -0800

    Added new action codes and PyMongo wrapper.

--- eureka/utils/service.py
+++ eureka/utils/service.py
@@ -9,14 +9,28 @@ from pymongo import MongoClient
 
 class MongoService:
     def __init__(self, db_url: str = MONGODB_CONN_STR):
-        self.mdb_client = MongoClient(db_url)
-        self.ee_db = self.mdb_client.get_database('EurekaEats')
-    
+        try:
+            self.mdb_client = MongoClient(db_url)
+        except Exception as db_exception:
+            print(f'{__name__}: {db_exception}')
+            self.mdb_client = None
+        finally:
+            if self.mdb_client is not None:
+                self.ee_db = self.mdb_client.get_database('EurekaEats')
+            else:
+                self.ee_db = None
+
     def close_service(self):
+        if self.mdb_client is None:
+            return
+        
         self.mdb_client.close()
     
-    def get_collection(self, collection_name: str = ""):
-        if not collection_name:
+    def is_ready(self):
+        return self.ee_db is not None
+
+    def get_collection(self, collection_name: str = ''):
+        if not collection_name or not self.ee_db:
             return None
 
         return self.ee_db.get_collection(collection_name)
