a854c65d1f1e34f83c3c10c05c5b24f27ac40d85
drkwitht <derektan3510@gmail.com>
2023-11-18 10:11:18 -0800

    Fixed small typos and cleaned imports for App.jsx

--- eurekaeats/src/App.jsx
+++ eurekaeats/src/App.jsx
@@ -1,6 +1,4 @@
-
 import React from 'react';
-// import Navigator from '../src/Navigator/Navigator';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 import LogIn from './LogIn/LogIn';
@@ -8,14 +6,13 @@ import SignIn from './SignIn/SignIn';
 
 import useJToken from './utils/useJToken';
 
-// TODO: complete SignIn page component functionality... -DrkWithT
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage/>}>
         </Route>
-        <Route path="login" element={<LogIn useJTokenHook={useJToken}/>}></Route>
+        <Route path="/login" element={<LogIn useJTokenHook={useJToken}/>}></Route>
         <Route path="/signin" element={<SignIn useJTokenHook={useJToken}/>}></Route>
       </Routes>
     </BrowserRouter>
