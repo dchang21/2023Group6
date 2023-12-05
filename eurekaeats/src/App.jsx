8e0e17dcf42b269b1c586c5d598d9a9c7dfa0b9f
drkwitht <derektan3510@gmail.com>
2023-12-04 23:12:21 -0800

    Temporary import and comment fix for App.jsx, more TBA.

--- eurekaeats/src/App.jsx
+++ eurekaeats/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import useJToken from './utils/useJToken';
+
 import LandingPage from './LandingPage/LandingPage';
 import LogIn from './LogIn/LogIn';
 import SignIn from './SignIn/SignIn';
-
 import ProfilePage from './ProfilePage/ProfilePage';
-
-import useJToken from './utils/useJToken';
-import AccountInfo from './AccountInfo/AccountInfo';
 import HomePage from './HomePage/HomePage';
 import Favorites from './Favorites/Favorites';
-import RestaurantComponent from './RestaurantComponent/RestaurantComponent';
+// import RestaurantComponent from './RestaurantComponent/RestaurantComponent';
+
+/// TODO: 1. Implement search component.
+/// TODO: 2. Implement single restaurant view page. 
 
 function App() {
   return (
@@ -19,10 +20,9 @@ function App() {
         <Route path="/" element={<LandingPage/>}></Route>
         <Route path="/login" element={<LogIn useJTokenHook={useJToken}/>}></Route>
         <Route path="/signin" element={<SignIn useJTokenHook={useJToken}/>}></Route>
-        <Route path="/accountinfo" element={<AccountInfo useJTokenHook={useJToken}/>}></Route>
         <Route path="/home" element={<HomePage useJTokenHook={useJToken}/>}></Route>
         <Route path="/favorites" element={<Favorites useJTokenHook={useJToken}/>}></Route>
-        <Route path="/restaurant" element={<RestaurantComponent useJTokenHook={useJToken}/>}></Route>
+        {/* <Route path="/restaurant/:id" element={<RestaurantComponent useJTokenHook={useJToken}/>}></Route> */}
         <Route path="/login" element={<LogIn usedJTokenHook={useJToken}/>}></Route>
         <Route path="/signin" element={<SignIn usedJTokenHook={useJToken}/>}></Route>
         <Route path='/profile' element={<ProfilePage usedJTokenHook={useJToken}/>}></Route>
